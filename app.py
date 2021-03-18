6e627ad91c3aac85650c0c4bf821b91eba9a33c3
Robert Owens <robertowens@Roberts-iMac.local>
2021-03-18 17:26:44 -0400

    starting to implement the auth endpoints

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, db_create_all, return_db, Item, List, User
+from auth import requires_auth
 
 def create_app(test_config=None):
   # create and configure the app
@@ -23,13 +24,37 @@ def after_request(response):
 Routes 
 '''
 #--------------------------Get METHODS------------------------#
+'''
+GET / endpoint
+    This is a public endpoint that represents the list model with the short() description method.
+    This returns status code 200 and json {"success": True, "lists": lists} where list is the
+    list of lists.
+Returns:
+    Status code 200 and list of lists.
+'''
 @app.route('/', methods=['GET'])
 def all_lists():
   return 'index where lists are'
 
+'''
+GET /my_lists endpoint
+    This is an endpoint that requires the 'get:lists' permission.  Once the action is authorized
+    the method with retrieve a list of lists, in their long description format, from the database.
+Requires:
+    'get:lists' permission
+Returns:
+    Status code 200 and json {"success": True, "lists": lists} where lists is the list of user lists.
+Known errors:
+    401 Unauthorized if user lacks permission
+'''
+@app.route('/my_lists', method=['GET'])
+@requires_auth('get:lists')
+def my_lists(payload):
+  return 'auth implemented'
+
 @app.route('/add_item', method=['POST'])
 @requires_auth('post:item')
-def add_item():
+def add_item(payload):
   return 'auth implemented'
 
 @app.route('/contents')
--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = '.us.auth0.com'
+AUTH0_DOMAIN = 'fsnd-project3-borbert.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'http://localhost:5000'
+API_AUDIENCE = 'listapp'
 
 ## AuthError Exception
 '''
