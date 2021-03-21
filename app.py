423596938224b03ce346cfd86de20d5d2ce0d1c5
Robert Owens <robertowens@Roberts-iMac.local>
2021-03-21 07:35:26 -0400

    GET methods imnplemented

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, db_drop_and_create_all, setup_db, Actor, Movies
-from auth import requires_auth
+from auth import requires_auth, AuthError
 
 def create_app(test_config=None):
   # create and configure the app
@@ -47,22 +47,62 @@ Returns:
 Known errors:
     401 Unauthorized if user lacks permission
 '''
-@app.route('/my_lists', methods=['GET'])
-@requires_auth('get:lists')
-def my_lists(payload):
+@app.route('/actors', methods=['GET'])
+@requires_auth('get:actors')
+def get_actors(payload):
   return 'auth implemented'
 
-@app.route('/items', methods=['POST'])
-# @requires_auth('post:item')
-def add_item(payload):  #
-  pass
-    
-  
+@app.route('/movies', methods=['GET'])
+@requires_auth('get:movies')
+def get_moives(payload):  
+  return 'auth implemented'
 
 @app.route('/contents')
 # @requires_auth('post:store')
 def contents(payload):
   return 'contents implemented'
 
+#--------------------------Error Handling------------------------#
+@app.errorhandler(401)
+def not_authorized(error):
+  return jsonify({
+      "success": False,
+      "error": 401,
+      "message": "Authentication error."
+  }), 401
+
+@app.errorhandler(403)
+def forbidden(error):
+  return jsonify({
+      "success": False,
+      "error": 403,
+      "message": "Forbidden."
+  }), 403
+
+@app.errorhandler(404)
+def not_found(error):
+  return jsonify({
+      "success": False,
+      "error": 404,
+      "message": "Item not found."
+  }), 404
+
+@app.errorhandler(422)
+def unprocessable(error):
+  return jsonify({
+      "success": False,
+      "error": 422,
+      "message": "Request could not be processed."
+  }), 422
+
+@app.errorhandler(AuthError)
+def auth_error(error):
+  return jsonify({
+      'success': False,
+      'error': error.status_code,
+      'message': error.error['description']
+  }), error.status_code
+
+#--------------------------App Entry-----------------------------#
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
